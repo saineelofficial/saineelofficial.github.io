6fc5e55904bd9ee709216e2d00f47ce34ce0ef7b
Sai Neel <ysnchary@gmail.com>
2021-10-31 03:11:20 +0530

    Update script.js

--- flip_health/script.js
+++ flip_health/script.js
@@ -1,7 +1,7 @@
 
     
 $(document).ready(function(){
-    let match = window.matchMedia('(max-width:425px)')
+    let match = window.matchMedia('(max-width:430px)')
     $(".owl-carousel").owlCarousel({
         items: match.matches? 1 : 2,
         loop:true,
